9c8724c739ff4c871d9a7e2c024650bcfbc6542c
Mark Murray <markmurray406@gmail.com>
2013-05-27 20:06:38 +0100

    added heroku details

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mntrme::Application.config.secret_token = '5ecae22e157fb7830d4cdae7fd96645742606ddd9b16c61253e54ab673bdff7f4c8f03a317e41aa5e0133a05de6607f9f0fc03fccbdce8a71fb4e23dd62725c6'
